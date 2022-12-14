e47c194425827d36173d678a47c6ddd2fbd7e122
ila <ilariadance98@live.it>
2022-12-14 16:49:19 +0100

    css e cazzatine varie

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,28 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { finalize } from 'rxjs';
 import { Pokemon } from '../pokemon';
 import { PokemonService } from '../pokemon.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit{
+export class HomeComponent implements OnInit {
+  public poke: Pokemon | null = null;
 
-  poke: Pokemon | null = null;
+  public inputName: string = '';
 
-  constructor(private pokemonService: PokemonService) {
+  public isLoading = false;
 
-  }
+  constructor(private pokemonService: PokemonService) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   getPokemon(name: string): void {
+    this.isLoading = true;
     this.pokemonService.getPokemon(name)
-    .subscribe(poke => {
-      this.poke = poke;
-      console.log(poke);
-    });
+    .pipe(
+      //questo finalize è fatto in ogni caso della subscribe -> mentre aspetta che venga preso l'errore manda la scriatta LOADING
+      finalize(() => {this.isLoading = false;})
+    )
+    //la subscribe prende 3 termini -> next, error, complete
+    .subscribe(
+      (poke) => {
+        this.poke = poke;
+        console.log(poke);
+      },
+      (err) => {
+        this.poke = null;
+      }
+    );
+
+    this.inputName = '';
   }
 }
